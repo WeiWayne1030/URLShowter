b356807548e842d8bd7111af0af8ad9cbed3633e
WeiWayne1030 <wayne10032004@gmail.com>
2023-03-03 15:07:10 +0800

    fix:edit button

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,4 +7,51 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+ //短網址頁面導向原網址頁面
+router.get('/:name', (req, res) => {
+  const name = req.params.name
+  URL.find()
+    .lean()
+    .then(url => {
+      const searchResult = url.find(url => url.name === name)
+      const targetURL = searchResult.originalURL
+      res.redirect(targetURL)
+    })
+    .catch(err => {console.log(err)
+          res.render('error',{ error: err.message })
+    })
+})
+
+//修改shorten
+router.get('/:name/edit', (req, res) => {
+  const name = req.params.name
+  URL.find()
+    .lean()
+    .then((url) => {
+    const searchResult = url.find(url => url.name === name)
+    const shortenURL = searchResult.name
+    res.render('edit', {shortenURL} )})
+    .catch(err => {
+      console.log(err)
+      res.render(
+      'errorPage', 
+      { error: err.message }
+      )
+    })
+})
+
+router.post('/:name/edit', (req, res) => {
+ let Newurl = req.body.url
+const name = req.params.name
+URL.find()
+     .then(url => {
+      let searchResult = url.find(url => url.name === name)
+      let originalURL = searchResult.originalURL
+      originalURL = Newurl
+      searchResult.save(originalURL)
+     })
+     .then(()=> res.redirect('/'))
+     .catch(error => console.log(error))
+ })
+
 module.exports = router
