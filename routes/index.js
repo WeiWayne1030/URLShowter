08c8f80173a83b48a07c3d1bfc266823d5c31796
WeiWayne1030 <wayne10032004@gmail.com>
2023-03-03 10:36:13 +0800

    refactor: modify routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-const urls = require('./modules/urls')
 const generateURL = require('./modules/generateURL')
 
 router.use('/', home)
-router.use('/', urls)
 router.use('/generateURL', generateURL)
 
 module.exports = router
